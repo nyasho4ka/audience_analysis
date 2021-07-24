728a2cbc403c5c0ec4d660d4781c2eef568579ab
nyasho4ka <nyasho4ka@gmail.com>
2021-07-25 02:50:31 +0500

    feat: Add structure for Spotify API and audio meta data processing

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import argparse
 import time
 import os
 from multiprocessing import Pool
 
 import pandas as pd
 import numpy as np
-from models import GroupInfo, MembersGroupInfo
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from models import MembersGroupInfo
 from processor import VkProcessor
-from utils import get_phone_and_password, get_required_table_fields
+from utils import (
+    get_vk_credentials,
+    get_spotify_credentials,
+    get_required_table_fields,
+    get_args, timer)
 
 
-def worker(group_id_, offset_, user_fields_):
+def group_process_worker(group_id_, offset_, user_fields_):
     worker_id = os.getpid()
 
-    vk_processor_ = VkProcessor(*get_phone_and_password())
+    vk_processor_ = VkProcessor(*get_vk_credentials())
     vk_processor_.set_group_by_id(group_id=group_id_)
 
     members_group_info: MembersGroupInfo = vk_processor_.get_members_group_info(offset=offset_)
@@ -35,26 +40,15 @@ def worker(group_id_, offset_, user_fields_):
     pd.DataFrame(np.array(user_data), columns=user_fields_).to_csv(f'result_{offset_}_{offset_ + 1000}.csv')
 
     print(f'WORKER_ID={worker_id} - FINISH')
-    return 'ok'
+    return 0
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--group_id', dest='group_id', type=str, required=True,
-    )
-    args = parser.parse_args()
-
-    final_result = []
-    user_fields = get_required_table_fields()
-    start_time = time.time()
-    group_id = args.group_id
-
+def process_group(group_id: str, user_fields: list):
     with Pool(processes=4) as pool:
         async_results = []
-        for offset in range(0, 10000, 1000):
+        for offset in range(0, 10000, 1000):  # TODO make number of people configurable
             async_result = pool.apply_async(
-                func=worker,
+                func=group_process_worker,
                 args=(group_id, offset, user_fields)
             )
             async_results.append(async_result)
@@ -64,5 +58,55 @@ if __name__ == '__main__':
         for async_result in async_results:
             async_result.wait()
 
-    end_time = time.time()
-    print(f"TIME HAS PASSED: {end_time - start_time} s.")
+
+def audio_process_worker(audio_files):
+    """
+    Several audio files are not big enough to lead to memory leaks,
+    So we can just process them in parallel
+    :return:
+    """
+    audio_dfs = pd.concat((pd.read_csv(f'audio_data/{audio_file}' for audio_file in audio_files)))
+    return audio_dfs['artist'].str.split(', | feat. ', expand=True, n=1)[0].unique()
+
+
+def get_all_unique_artists():
+    audio_files = (filename for filename in os.listdir('audio_data') if '.csv' in filename)
+    audio_chunks = np.array_split(audio_files, os.cpu_count())
+    with Pool(processes=os.cpu_count()) as pool:
+        async_results = []
+        for audio_chunk in audio_chunks:
+            async_results.append(pool.apply_async(
+                audio_process_worker,
+                args=(audio_chunk,)
+            ))
+
+        for async_result in async_results:
+            async_result.wait()
+
+    return set().union(*async_results)
+
+
+def get_all_artists_info(all_unique_artists):
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(*get_spotify_credentials()))
+    # Get all artists meta info
+    # return it and create dataframe
+    return []
+
+def process_audio():
+    all_unique_artists = get_all_unique_artists()
+    all_artists_info = get_all_artists_info(all_unique_artists)
+    pd.to_csv(all_artists_info)
+
+
+@timer
+def main():
+    args = get_args()
+    user_fields = get_required_table_fields()
+    group_id = args.group_id
+
+    process_group(group_id, user_fields)
+    process_audio()
+
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,13 +1,36 @@
 import json
+import time
+import argparse
 
 with open('config.json') as config:
     CONFIG = json.loads(config.read())
 
 
-def get_phone_and_password():
-    credentials = CONFIG['credentials']
+def get_vk_credentials():
+    credentials = CONFIG['vk']['credentials']
     return credentials['phone'], credentials['password']
+
+
+def get_spotify_credentials():
+    credentials = CONFIG['spotify']['credentials']
+    return credentials['client_id'], credentials['client_secret']
 
 
 def get_required_table_fields():
     return CONFIG['csv_processing']['fields']
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--group_id', dest='group_id', type=str, required=True,
+    )
+    return parser.parse_args()
+
+
+def timer(fn):
+    def wrapper():
+        start_time = time.time()
+        fn()
+        print(f"TIME HAS PASSED: {time.time() - start_time} s.")
+    return wrapper
